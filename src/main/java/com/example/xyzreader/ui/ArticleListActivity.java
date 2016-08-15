3f39cdc16de17e845e7bcab0953d3bc0980ed11b
Paul <paulgureghian@att.net>
2016-08-15 12:52:32 -0700

    8-15-16  12:50  PM

--- src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -14,8 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
-import android.util.TypedValue;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -37,6 +36,8 @@ public class ArticleListActivity extends ActionBarActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
 
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
         final View toolbarContainerView = findViewById(R.id.toolbar_container);
