ab4777977b2fe35cd3d8a5228cf5396d4984f5b4
Paul <paulgureghian@att.net>
2016-08-21 16:46:39 -0700

    8-21- 16  4:45  PM

---
+++ src/main/java/com/example/xyzreader/ui/ThreeTwoImageView.java
@@ -0,0 +1,30 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ThreeTwoImageView extends ImageView {
+
+    public ThreeTwoImageView(Context context) {
+        super(context);
+
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    @Override
+    protected void onMeasure (int widthSpec, int heightSpec) {
+        int threeTwoHeight = MeasureSpec.getSize(widthSpec)*2/3;
+        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthSpec, threeTwoHeightSpec);
+
+    }
+}
+
